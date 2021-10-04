4c508393a3606a70d42722b0b2731dabf3bd7710
khimt <68567562+khimt@users.noreply.github.com>
2021-10-04 21:50:06 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\WorkersController;
 use App\Http\Controllers\WalletController;
+use App\Http\Controllers\OverclockController;
+use App\Http\Controllers\GpuController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,11 +23,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//
 Route::get('/test', function (Request $request) {
     return 'test';
 });
-
 
 //Authentification
 Route::post('register',[UserController::class, 'register']);
@@ -39,12 +39,30 @@ Route::post('/workers/init', [WorkersController::class, 'initWorkers']);
 Route::post('/workers/checkExist', [WorkersController::class, 'ifExist']);
 Route::put('get/workers/url', [WorkersController::class, 'getURL']);
 Route::middleware('auth:api')->group(function() {
+    Route::delete('workers/{id}', [WorkersController::class, 'deleteWorkers']);
     Route::put('workers/connect', [WorkersController::class, 'workersConnect']);
-    Route::get('/workers/{id}', [WorkersController::class, 'listWorkers']);
-    // Route::put('/workers/{id}/update', [WorkersController::class, 'updateWorkers']);
+    Route::put('workers/disconnect', [WorkersController::class, 'workersDisconnect']);
+    Route::get('/workers', [WorkersController::class, 'listWorkers']);
+    Route::put('/workers/{id}/update', [WorkersController::class, 'updateWorkers']);
 });
 
 Route::middleware('auth:api')->group(function() {
     Route::post('wallet', [WalletController::class, 'wallet']);
-    Route::get('/wallet/{id}', [WalletController::class, 'listWallet']);
+    Route::get('wallets', [WalletController::class, 'listWallet']);
+    Route::delete('wallet/{id}', [WalletController::class, 'delete']);
+});
+
+Route::get('/worker/gpu/overclock', [OverclockController::class, 'getWorkerGpuOverclock']);
+Route::middleware('auth:api')->group(function() {
+    Route::post('overclock', [OverclockController::class, 'create']);
+    Route::get('overclock', [OverclockController::class, 'get']);
+    Route::put('overclock', [OverclockController::class, 'update']);
+});
+
+Route::post('/gpu/init', [GpuController::class, 'initGpu']);
+Route::middleware('auth:api')->group(function() {
+    Route::get('/gpu/{id}', [GpuController::class, 'getGpu']);
+    Route::put('/gpu/{id}', [GpuController::class, 'updateGpu']);
+    Route::get('/gpus', [GpuController::class, 'getList']);
+    Route::get('/hardware/list', [GpuController::class, 'getHardwareList']);
 });
